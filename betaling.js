fa7675734f381952987d81dc1c58cfefdc9b66b0
Juliesand2810 <eaa24jus@students.eaaa.dk>
2025-12-12 13:46:08 +0100

    betalingside koblet sammen med timer og nye sider

--- betaling.js
+++ betaling.js
@@ -13,8 +13,25 @@ betalBtn.onclick = function () {
 
    // skjul annuller-knap
   annullerBtn.style.display = "none";
+
+  // send videre efter 2 sekunder til tak-siden
+  setTimeout(function () {
+    window.location.href = "tak.html"; 
+  }, 2000);
 };
 
 annullerBtn.onclick = function () {
   alert("Betaling annulleret");
+
+  // send videre efter 2 sekunder (starter efter alert lukkes)
+  setTimeout(function () {
+    window.location.href = "index.html";
+  }, 2000);
 };
+
+
+
+
+
+
+
